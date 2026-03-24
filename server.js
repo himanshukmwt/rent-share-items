e2791a25151fd86251917e206703a8f1d75c1241
himanshukmwt <himanshukumawat8824@gmail.com>
2026-03-24 22:29:32 +0530

    rate update

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(cors({
 }));
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
   message: "Too many requests, please try again later"
 });
 app.use("/api", limiter);
